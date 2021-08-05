f4474e356bbe316c4223171137d9abdd2f985587
LIMITDadmin <51476983+LIMITDadmin@users.noreply.github.com>
2021-08-06 00:23:36 +0200

    db config change

--- routes/new-index.js
+++ routes/new-index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
 		var dbo = db.db("admin");
-		dbo.collection("collection1").find({}).toArray(function(err, result) {
+		dbo.collection("heros").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log('Mongo data coming in hot')
     		console.log(result);
