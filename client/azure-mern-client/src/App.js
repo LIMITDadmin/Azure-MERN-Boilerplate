f6869c330013f98f6b269a27cbdbea372ef8e2d9
LIMITDadmin <51476983+LIMITDadmin@users.noreply.github.com>
2021-08-06 00:17:52 +0200

    back to initial

--- client/azure-mern-client/src/App.js
+++ client/azure-mern-client/src/App.js
@@ -65,7 +65,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div>this is running</div>
         <ul>
           {
             Object.keys(this.state.bestShows).map((cur, idx) => (
