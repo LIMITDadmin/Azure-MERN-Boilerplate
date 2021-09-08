9a948ce83cd3eeb16ff1e4cfe4ac8f4a7cfff285
LIMITDadmin <51476983+LIMITDadmin@users.noreply.github.com>
2021-09-08 18:29:32 +0200

    trying bugfix with mongoose connect statement

--- routes/HeroModel.js
+++ routes/HeroModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://cosmo-limitd:1ucTvssvrmgTZk4Y2XlTazV6gqLzg8XIEgEmEemaGq0xgBz0f7TjdGC5RfB5jpC24gpbA8xlDsxy7GP6Shf4Fg==@cosmo-limitd.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmo-limitd@');
+
 const heroSchema = new Schema(
   {
     type: String,
