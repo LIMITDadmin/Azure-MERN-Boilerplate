fa72b3a9f44538ad5929173da50602ed1d73b6cc
LIMITDadmin <51476983+LIMITDadmin@users.noreply.github.com>
2021-09-08 23:10:40 +0200

    bugfix empty body

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 var app = express();
 
@@ -12,9 +13,14 @@ app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 5000);
 console.log("+++++++++++++++" + app.get('port'));
 
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static('./client/build'));
